2d146801c43c5eeacd2926f0c96d04493cacc5f7
Michael Berber <mickberber@icloud.com>
2017-03-11 23:00:55 -0800

    handle errors, naming fix

--- hackernews_reader.py
+++ hackernews_reader.py
@@ -13,37 +13,41 @@ def headlines(storylinks):
     while i < len(storylinks):
         print str(i + 1) + '. ' + storylinks[i][1]
         i += 1
-    return
 
 def openpage(storylinks, index):
-    url = storylinks[index][0]
+    url = storylinks[index - 1][0]
     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
     webbrowser.get(chrome_path).open(url)
 
+def handle_error():
+    print 'Usage: [--headlines -h] [--open -o][headline number] [--copy -cp][filename]'
+    sys.exit(1)
 
 def main():
-
-
-    if len(sys.argv) > 1:
-        argument = sys.argv[1]
+    arguments = sys.argv
+    if len(arguments) > 1:
         currentdir = os.path.abspath('.')
         uf = urllib.urlopen('http://news.ycombinator.com')
         htmlfile = uf.read()
         storylinks = re.findall(r'href="(.+)" class="storylink">(.+)</a><span', htmlfile)
 
-        if argument == '--headlines' or argument == '-h':
+        if arguments[1] == '--headlines' or arguments[1] == '-h':
             headlines(storylinks)
 
-        if argument == '--open' or argument == '-o':
-            openpage(storylinks, int(sys.argv[2]))
+        if arguments[1] == '--open' or arguments[1] == '-o':
+            if len(arguments) > 2:
+                openpage(storylinks, int(arguments[2]))
+            else:
+                handle_error()
 
-        else:
-            writefile = open(currentdir + '/' + sys.argv[1], 'w+')
-            writefile.write(htmlfile)
+        if arguments[1] == '--copy' or arguments[1] == '-cp':
+            if len(arguments) > 2:
+                writefile = open(currentdir + '/' + arguments[2], 'w+')
+                writefile.write(htmlfile)
+            else:
+                handle_error()
     else:
-        print 'Usage: [--headlines -h] [-cp htmlfile]'
-        sys.exit(1)
-    return
+        handle_error()
 
 
 if __name__ == '__main__':
