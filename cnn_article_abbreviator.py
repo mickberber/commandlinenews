2108144d6c53bd52bec6a8a26248b09100048d29
Michael Berber <mickberber@icloud.com>
2017-03-12 11:25:29 -0700

    grab article bodys in cnn abbrev

--- cnn_article_abbreviator.py
+++ cnn_article_abbreviator.py
@@ -10,9 +10,17 @@ def main():
     currentdir = os.path.abspath('.')
     uf = urllib.urlopen(arguments[1])
     htmlfile = uf.read()
-    storylinks = re.findall(r'storyhighlights__list">(.+?)</ul>', htmlfile)
-    # print storylinks
-    print re.findall(r'normal">(.+?)</li>', storylinks[0])
+    highlights = re.findall(r'storyhighlights__list">(.+?)</ul>', htmlfile)
+    highlights = re.findall(r'normal">(.+?)</li>', highlights[0])
+    content = re.findall(r'body__paragraph">(.+?)</', htmlfile)
+    print '=== Story Hightlights ==='
+    for hl in highlights:
+        print hl
+    print '=== === === ==='
+    print content[0][-5:]
+    del content[0]
+    for p in content:
+        print p
     return
 
 if __name__ == '__main__':
