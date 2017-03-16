123bfb09e35e9b1ebd56c4aabf223f545e3e4ad3
Michael Berber <mickberber@icloud.com>
2017-03-15 22:33:26 -0700

    add bbc & twitter local html files

--- bbc.py
+++ bbc.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
+import os
+import utils
 
 def main():
+    currentdir = os.path.abspath('.')
+    f = open(currentdir + '/test/bbc.html', 'rU')
+    print f.read()
     return
 
 if __name__ == '__main__':
--- twitter.py
+++ twitter.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
+import os
 
 def main():
+    currentdir = os.path.abspath('.')
+    f = open(currentdir + '/test/twitter.html', 'rU')
+    print f.read()
     return
 
 if __name__ == '__main__':
