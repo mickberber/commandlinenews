03361ae940a25cc514324207765b1c6e6584c0ea
Michael Berber <mickberber@icloud.com>
2017-03-15 22:37:19 -0700

    grab bbc news instead of hp

--- bbc.py
+++ bbc.py
@@ -4,6 +4,8 @@ import utils
 
 def main():
     currentdir = os.path.abspath('.')
+    htmlfile = utils.get_html_file('http://www.bbc.com/news')
+    utils.copy_file('test/bbc.html', htmlfile)
     f = open(currentdir + '/test/bbc.html', 'rU')
     print f.read()
     return
